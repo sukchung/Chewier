20b1d523969c69dd8d201a2d29780511aad8e505
adrien <adrienchan56@gmail.com>
2023-01-10 16:22:24 -0800

    added pets queries/routers

--- accounts/main.py
+++ accounts/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from routers import accounts
+from routers import accounts, pets
 
 app = FastAPI()
 app.include_router(accounts.router)
+app.include_router(pets.router)
