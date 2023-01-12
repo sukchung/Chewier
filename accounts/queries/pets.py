921c44220a1c0e3e08ae17b8b71ca338f2ec3413
Adrien Chan <adrienchan56@gmail.com>
2023-01-12 23:10:42 +0000

    Accounts Backend to Main

---
+++ accounts/queries/pets.py
@@ -0,0 +1,161 @@
+from pydantic import BaseModel
+from queries.pool import pool
+from typing import List, Union, Optional
+
+
+class Error(BaseModel):
+    message: str
+
+
+class PetIn(BaseModel):
+    name: str
+    breed: str
+    size: str
+    age: int
+    account_id: int
+
+
+class PetOut(BaseModel):
+    id: int
+    name: str
+    breed: str
+    size: str
+    age: int
+    account_id: int
+
+
+class PetRepository:
+    def create(self, pet: PetIn) -> PetOut:
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
+                    INSERT INTO pets
+                        (name, breed, size, age, account_id)
+                    VALUES
+                        (%s, %s, %s, %s, %s)
+                    RETURNING id;
+                    """,
+                    [pet.name, pet.breed, pet.size, pet.age, pet.account_id],
+                )
+                id = result.fetchone()[0]
+                return self.pet_in_to_out(id, pet)
+
+    def get_all_pets(self) -> Union[List[PetOut], Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id
+                        , name
+                        , breed
+                        , size
+                        , age
+                        , account_id
+                        FROM pets
+                        ORDER BY id;
+                        """
+                    )
+                    return [
+                        PetOut(
+                            id=record[0],
+                            name=record[1],
+                            breed=record[2],
+                            size=record[3],
+                            age=record[4],
+                            account_id=record[5],
+                        )
+                        for record in result
+                    ]
+        except Exception as e:
+            # print(e) print 'em if you want 'em during testing
+            return {
+                "message": "Could not retrieve all pets.  Please try your request again."
+            }
+
+    def update_pet(self, pet_id: int, pet: PetIn) -> Union[PetOut, Error]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        UPDATE pets
+                        SET name = %s
+                        , breed = %s
+                        , size = %s
+                        , age = %s
+                        , account_id = %s
+                        WHERE id = %s
+                        """,
+                        [
+                            pet.name,
+                            pet.breed,
+                            pet.size,
+                            pet.age,
+                            pet.account_id,
+                            pet_id
+                        ]
+                    )
+                    return self.pet_in_to_out(pet_id, pet)
+
+        except Exception as e:
+            return {"message": "Could not update that pet; please try again."}
+
+    def delete_pet(self, pet_id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE FROM pets
+                        WHERE id = %s
+                        """,
+                        [pet_id],
+                    )
+                    return True
+        except Exception as e:
+            return False
+
+    # SKIP AND CHECK PET_IN_TO_OUT FUNCTION
+    # FOR PET DETAIL STRETCH GOAL; NOT PART OF MVP
+
+    # def get_one_pet(self, pet_id: int) -> Optional[PetOut]:
+    #     try:
+    #         with pool.connection() as conn:
+    #             with conn.cursor() as db:
+    #                 result = db.execute(
+    #                     """
+    #                     SELECT id
+    #                     , name
+    #                     , breed
+    #                     , size
+    #                     , age
+    #                     , account_id
+    #                     FROM pets
+    #                     WHERE id = %s
+    #                     """,
+    #                     [pet_id]
+    #                 )
+    #                 record = result.fetchone()
+    #                 if record is None:
+    #                     return None
+    #                 return self.record_to_pet_out(record)
+    #     except Exception as e:
+    #         return {"message": "Could not locate that pet.  Please try again."}
+
+    def pet_in_to_out(self, id: int, pet: PetIn):
+        old_data = pet.dict()
+        return PetOut(id=id, **old_data)
+
+
+# FOR PET DETAIL STRETCH GOAL; NOT PART OF MVP
+# def record_to_pet_out(self, record):
+#     return PetOut(
+#         id=record[0],
+#         name=record[1],
+#         breed=record[2],
+#         size=record[3],
+#         age=record[4],
+#         account_id=record[5]
+#     )
---
+++ accounts/routers/pets.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, Depends, Response
+from queries.pets import PetIn, PetOut, PetRepository, Error
+from typing import Union, List, Optional
+
+router = APIRouter()
+
+@router.post("/pets", response_model=Union[PetOut,Error])
+def create_pet(
+    pet: PetIn,
+    response: Response,
+    repo: PetRepository = Depends(),
+):
+    # response.status_code = 400
+    return repo.create(pet)
+
+@router.get('/pets', response_model=Union[List[PetOut], Error])
+def get_all_pets(
+    repo: PetRepository = Depends(),
+):
+    return repo.get_all_pets()
+
+@router.put("/pets/{pet_id}", response_model=Union[PetOut, Error])
+def update_pet(
+    pet_id: int,
+    pet: PetIn,
+    repo: PetRepository = Depends(),
+) -> Union[PetOut, Error]:
+    return repo.update_pet(pet_id, pet)
+
+@router.delete('/pets/{pet_id}', response_model = bool)
+def delete_pet(
+    pet_id: int,
+    repo:PetRepository = Depends(),
+) -> bool:
+    return repo.delete_pet(pet_id)
+
+# @router.get('/pets/{pet_id}', response_model=Optional)
+# def get_one_pet(
+#     pet_id: int,
+#     response: Response,
+#     repo: PetRepository = Depends(),
+# ) -> PetOut:
+#     pet=repo.get_one(pet)
+#     if pet is None:
+#         response.status_code = 404
+#     return pet
