59c71a3c6b064fed75125feb57ff82d0d117e26a
adrien <adrienchan56@gmail.com>
2023-01-12 12:49:14 -0800

    backend changes

--- accounts/queries/accounts.py
+++ accounts/queries/accounts.py
@@ -109,71 +109,46 @@ class AccountRepository:
         except Exception as e:
             return {"message": "Could not locate that account.  Please try again. "}
 
-    # def get_all_accounts(self) -> Union[Error, List[AccountOut]]:
-    #     try:
-    #         with pool.connection() as conn:
-    #             with conn.cursor() as db:
-    #                 result = db.execute(
-    #                     """
-    #                     SELECT id, first_name, last_name, email, password, address
-    #                     FROM accounts
-    #                     ORDER BY id;
-    #                     """
-    #                 )
-    #                 return [
-    #                     self.record_to_account_out(record)
-    #                     for record in result
-    #                 ]
-    #     except Exception as e:
-    #         return {"message": "Could not retrieve all accounts.  Please try again."}
+    def get_all_accounts(self) -> Union[Error, List[AccountOut]]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id, first_name, last_name, email, address, hashed_password
+                        FROM accounts
+                        ORDER BY id;
+                        """
+                    )
+                    return [
+                        self.record_to_account_out(record)
+                        for record in result
+                    ]
+        except Exception as e:
+            return {"message": "Could not retrieve all accounts.  Please try again."}
 
-    # def update_an_account(
-    #     self, account_id: int, account: AccountIn
-    # ) -> Union[AccountOut, Error]:
-    #     try:
-    #         with pool.connection() as conn:
-    #             with conn.cursor() as db:
-    #                 db.execute(
-    #                     """
-    #                     UPDATE accounts
-    #                     SET first_name = %s
-    #                       , last_name = %s
-    #                       , email = %s
-    #                       , password = %s
-    #                       , address = %s
-    #                     WHERE id = %s
-    #                     """,
-    #                     [
-    #                         account.first_name,
-    #                         account.last_name,
-    #                         account.email,
-    #                         account.password,
-    #                         account.address,
-    #                         account_id
-    #                     ]
-    #                 )
-    #                 return self.account_in_to_out(account_id, account)
-    #     except Exception as e:
-    #         # print(e) if you want to print in testing
-    #         return {
-    #             "message": "Could not update that account.  Please check your input information, and try again."
-    #         }
-
-    # def delete_account(self, account_id: int) -> bool:
-    #     try:
-    #         with pool.connection() as conn:
-    #             with conn.cursor() as db:
-    #                 db.execute(
-    #                     """
-    #                     DELETE FROM accounts
-    #                     WHERE id = %s
-    #                     """,
-    #                     [account_id]
-    #                 )
-    #                 return True
-    #     except Exception as e:
-    #         # print(e)
-    #         return False
+    def delete_account(self, account_id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        DELETE FROM pets
+                        WHERE account_id = %s
+                        """,
+                        [account_id]
+                    )
+                    db.execute(
+                        """
+                        DELETE FROM accounts
+                        WHERE id = %s
+                        """,
+                        [account_id]
+                    )
+                    return True
+        except Exception as e:
+            # print(e)
+            return False
 
     # def account_in_to_out(self, id: int, account: AccountIn):
     #     old_data = account.dict()
