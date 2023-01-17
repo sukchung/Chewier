6b42a81abbe2749edd3274c46afc7b1eb690d087
adrien <adrienchan56@gmail.com>
2023-01-17 12:28:00 -0800

    new branch

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,37 +1,53 @@
-import { useEffect, useState } from 'react';
-import Construct from './Construct.js'
-import ErrorNotification from './ErrorNotification';
-import './App.css';
+// import { useEffect, useState } from 'react';
+// import Construct from './Construct.js'
+// import ErrorNotification from './ErrorNotification';
+// import './App.css';
 
-function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);  
+// function App() {
+//   const [launch_info, setLaunchInfo] = useState([]);
+//   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
+//   useEffect(() => {
+//     async function getData() {
+//       let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
+//       console.log('fastapi url: ', url);
+//       let response = await fetch(url);
+//       console.log("------- hello? -------");
+//       let data = await response.json();
 
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, [])
+//       if (response.ok) {
+//         console.log("got launch data!");
+//         setLaunchInfo(data.launch_details);
+//       } else {
+//         console.log("drat! something happened");
+//         setError(data.message);
+//       }
+//     }
+//     getData();
+//   }, [])
 
 
+//   return (
+//     <div>
+//       <ErrorNotification error={error} />
+//       <Construct info={launch_info} />
+//     </div>
+//   );
+// }
+
+// export default App;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./MainPage";
+
+function App(props) {
   return (
-    <div>
-      <ErrorNotification error={error} />
-      <Construct info={launch_info} />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
