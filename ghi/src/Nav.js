92078b15eb09b1dc7921dd72a27130f5c3121277
Suk Chung <sukchung7@gmail.com>
2023-01-22 17:29:38 -0800

    Added logout feature

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,11 +1,32 @@
 // Dependencies
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useToken } from "./Auth.js";
 
 // CSS
 import "./Styles/Nav.css";
 import addtocart from "./Images/addtocart.png";
 
 export default function Nav() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [token, , logout] = useToken();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  }, [token]);
+
+  function handleLogout(event) {
+    logout();
+    navigate("/");
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-purple">
       <div className="container-fluid">
@@ -30,15 +51,17 @@ export default function Nav() {
             </li>
           </ul>
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink
-                className="nav-link active"
-                aria-current="page"
-                to="/signup"
-              >
-                Sign up
-              </NavLink>
-            </li>
+            {!loggedIn && (
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/signup"
+                >
+                  Sign up
+                </NavLink>
+              </li>
+            )}
             <li className="nav-item">
               <NavLink
                 className="nav-link active"
@@ -48,6 +71,23 @@ export default function Nav() {
                 <img src={addtocart} id="resize" className="icon" alt="icon" />
               </NavLink>
             </li>
+            {loggedIn ? (
+              <li className="nav-item">
+                <button onClick={handleLogout} className="logout-button">
+                  Logout
+                </button>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/login"
+                >
+                  Login
+                </NavLink>
+              </li>
+            )}
           </ul>
         </div>
       </div>
