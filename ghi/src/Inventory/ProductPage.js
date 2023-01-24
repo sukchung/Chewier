3de7f5825df2f39ddb4e1f05dde4360f373acd99
Suk Chung <sukchung7@gmail.com>
2023-01-24 00:13:40 +0000

    Deployment

---
+++ ghi/src/Inventory/ProductPage.js
@@ -0,0 +1,48 @@
+// Dependencies
+import React, { useState, useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
+
+// Components
+import ProductCard from "./ProductCard";
+
+//CSS
+import "../Styles/ProductPage.css";
+import petbanner from "../Images/petbanner.jpg";
+
+export default function ProductPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const url = `${process.env.REACT_APP_INVENTORY_HOST}/products`;
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        setProducts(data);
+      }
+    }
+    getProducts();
+  }, []);
+
+  return (
+    <div className="products-page">
+      <div className="banner-container">
+        <img
+          src={petbanner}
+          className="center-banner"
+          alt="purple banner with dog"
+        />
+        <div className="center-text">Chewier Picks</div>
+      </div>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={4} lg={3} key={product.id}>
+            <div className="product-card">
+              <ProductCard product={product} />
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
