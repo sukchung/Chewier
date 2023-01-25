324bbb436b57ba7e34cb2c868155f5cf2e6e0a58
Alexander McKelvey <alexander.t.mckelvey@gmail.com>
2023-01-25 19:13:51 +0000

    Products

--- ghi/src/Inventory/ProductPage.js
+++ ghi/src/Inventory/ProductPage.js
@@ -5,12 +5,25 @@ import { Col, Row } from "react-bootstrap";
 // Components
 import ProductCard from "./ProductCard";
 
+
 //CSS
 import "../Styles/ProductPage.css";
 import petbanner from "../Images/petbanner.jpg";
 
+const getFilteredItems = (query, items) => {
+  if (!query) {
+    return items;
+  }
+  return items.filter((product) => product.name.includes(query))
+}
+
+
 export default function ProductPage() {
   const [products, setProducts] = useState([]);
+  const [query, setQuery] = useState('');
+  const filteredItems = getFilteredItems(query, products)
+  const [copyProducts, setCopyProducts] = useState([]);
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -21,11 +34,25 @@ export default function ProductPage() {
       if (response.ok) {
         const data = await response.json();
         setProducts(data);
+        setCopyProducts(data);
       }
       setLoading(false);
     }
     getProducts();
   }, []);
+
+  const handleBtns = (e) =>{
+  let word = e.target.value;
+  if (word === "All") {
+     setProducts(copyProducts)
+  } else if (word === "Dry") {
+     const filtered = products.filter(item => item.state === "Dry")
+     setProducts(filtered)
+  } else if (word === "Wet") {
+     const filtered = products.filter(item => item.state === "Wet")
+     setProducts(filtered)
+  }
+ }
 
   if (loading) {
     return (
@@ -45,17 +72,40 @@ export default function ProductPage() {
         />
         <div className="center-text">Chewier Picks</div>
       </div>
+      <div>
+        <label>Search</label>
+        <input type="text" onChange={e => setQuery(e.target.value)} />
+      </div>
+      <div className = "btns">
+      <button value="All" onClick={handleBtns}>
+          All
+        </button>
+        <button value="Dry" onClick={handleBtns}>
+          Dry
+        </button>
+        <button value="Wet" onClick={handleBtns}>
+          Wet
+        </button>
+      </div>
       <Row>
-        {Array.isArray(products) &&
-          products.length > 0 &&
-          products.map((product) => (
-            <Col sm={12} md={4} lg={3} key={product.id}>
-              <div className="product-card">
-                <ProductCard product={product} />
-              </div>
-            </Col>
-          ))}
+        {filteredItems.map((product) => (
+          <Col sm={12} md={3} lg={2} key={product.id}>
+            <div className="product-card">
+              <ProductCard product={product} id={product.id} />
+            </div>
+          </Col>
+        ))}
       </Row>
     </div>
   );
 }
+
+// {Array.isArray(products) &&
+//   products.length > 0 &&
+//   products.map((product) => (
+//     <Col sm={12} md={4} lg={3} key={product.id}>
+//       <div className="product-card">
+//         <ProductCard product={product} />
+//       </div>
+//     </Col>
+//   ))}
