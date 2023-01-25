f0be6b949975444937f429408052588435b26476
Alexander <alexander.t.mckelvey@gmail.com>
2023-01-25 12:21:55 -0800

    test_alex

--- inventory/tests/test_suk.py
+++ inventory/tests/test_suk.py
@@ -4,9 +4,6 @@ from queries.inventory import (
     FoodBrandRepository,
     FoodBrandOut,
     FoodBrandIn,
-    FoodProductRepository,
-    FoodProductOut,
-    FoodProductIn,
 )
 
 
@@ -33,42 +30,3 @@ def test_create_food_brand():
     response = client.post("/brands", json=food_brand_in.dict())
     assert response.status_code == 200
     assert response.json() == expected_food_brand.dict()
-
-
-class FakeFoodProductRepository:
-    def create(self, product):
-        return FoodProductOut(
-            id=1,
-            name=product.name,
-            price=product.price,
-            main_ingredient=product.main_ingredient,
-            brand_id=product.brand_id,
-            state=product.state,
-            picture_url=product.picture_url,
-        )
-
-
-def test_create_product():
-    product_in = FoodProductIn(
-        name="chewier",
-        price="1000",
-        main_ingredient="blue",
-        brand_id=1,
-        state="dry",
-        picture_url="None",
-    )
-    expected_product = FoodProductOut(
-        id=1,
-        name="chewier",
-        price="1000",
-        main_ingredient="blue",
-        brand_id=1,
-        state="dry",
-        picture_url="None",
-    )
-    app.dependency_overrides[FoodProductRepository] = FakeFoodProductRepository
-
-    response = client.post("/products", json=product_in.dict())
-
-    assert response.status_code == 200
-    assert response.json() == expected_product.dict()
