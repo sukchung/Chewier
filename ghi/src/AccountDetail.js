522de55270f03c657e8f33479abb1440a465225d
Alexander <alexander.t.mckelvey@gmail.com>
2023-01-26 12:33:13 -0800

    account detail styling

--- ghi/src/AccountDetail.js
+++ ghi/src/AccountDetail.js
@@ -1,23 +1,47 @@
-import { Link } from "react-router-dom";
+import { Link , useNavigate } from "react-router-dom";
 import { useAuthContext } from "./Auth";
-import "./index.css";
+import "./Styles/AccountDetail.css";
+import addpet from "./Images/addpet.jpg";
+import viewpet from "./Images/viewpet.jpg";
 
 function AccountDetail() {
   const { token } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleAdd = () => {
+    navigate('/petsform')
+  }
+
+  const handleView = () => {
+    navigate('/petslist')
+  }
+
   return (
     <>
-      <div>
-        <h1>Account Detail</h1>
-        <p>{token?.account.first_name}</p>
-        <p>{token?.account.last_name}</p>
-        <p>{token?.account.email}</p>
-        <p>{token?.account.address}</p>
-      </div>
-      <div>
-        <Link to="/petslist">View Your Pets</Link>
-      </div>
-      <div>
-        <Link to="/pets">Add Pet</Link>
+      <h1>Account Details</h1>
+      <div class="container">
+        <div class="info">
+          <h2>Name</h2>
+          <p class="text">{token?.account.first_name} {token?.account.last_name}</p>
+          <h2>Email</h2>
+          <p class="text">{token?.account.email}</p>
+          <h2>Address</h2>
+          <p class="text bottom-text">{token?.account.address}</p>
+        </div>
+        <div class="buttons">
+          <div class="add">
+            <div class="pic-container">
+              <img src={addpet}/>
+            </div>
+            <button type="button" class="btn btn-primary btn-lg" onClick={handleAdd}>Add A Pet</button>
+          </div>
+          <div class="view">
+            <div class="pic-container">
+              <img src={viewpet}/>
+            </div>
+            <button type="button" class="btn btn-primary btn-lg" onClick={handleView}>View Your Pets</button>
+          </div>
+        </div>
       </div>
     </>
   );
